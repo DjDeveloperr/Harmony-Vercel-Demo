00ae455978b1273b1a85ae7408d7d6930f35c02f
DjDeveloperr <djsandhu2005@gmail.com>
2021-03-14 14:21:05 +0530

    up

--- deps.ts
+++ deps.ts
@@ -1,2 +1,2 @@
 export * from "https://deno.land/std@0.90.0/http/server.ts";
-export * from "https://raw.githubusercontent.com/DjDeveloperr/harmony/slash/mod.ts";
+export * from "https://raw.githubusercontent.com/DjDeveloperr/harmony/fixes/mod.ts";
