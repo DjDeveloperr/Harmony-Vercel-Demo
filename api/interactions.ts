347bbc929d977a4b3ff0dc11141ad926b5fc43d1
DjDeveloperr <djsandhu2005@gmail.com>
2021-03-14 15:48:00 +0530

    up

--- api/interactions.ts
+++ api/interactions.ts
@@ -14,32 +14,9 @@ await slash.commands.bulkEdit([
 ]);
 
 export default async (req: ServerRequest) => {
-  if (req.method.toLowerCase() !== "post")
-    return req.respond({ status: 400, body: "method not allowed" });
+  const d = await slash.verifyServerRequest(req);
+  if (d === false) return req.respond({ status: 401, body: "not authorized" });
 
-  const data = await Deno.readAll(req.body);
-  const verify = slash.verifyKey(
-    data,
-    req.headers.get("x-signature-ed25519") ?? "",
-    req.headers.get("x-signature-timestamp") ?? ""
-  );
-  if (!verify) return req.respond({ status: 401, body: "not authorized" });
-
-  const respond = (data: any) =>
-    req.respond({
-      body: JSON.stringify(data),
-      headers: new Headers({
-        "content-type": "application/json",
-      }),
-    });
-
-  const interaction = JSON.parse(new TextDecoder("utf-8").decode(data));
-  if (interaction.type == 0) respond({ type: 0 });
-
-  respond({
-    type: 4,
-    data: {
-      content: "Pong!",
-    },
-  });
+  if (d.type === 1) return d.respond({ type: 1 });
+  d.reply("Pong!");
 };
--- deps.ts
+++ deps.ts
@@ -1,2 +1,2 @@
 export * from "https://deno.land/std@0.90.0/http/server.ts";
-export * from "https://raw.githubusercontent.com/DjDeveloperr/harmony/fixes/mod.ts";
+export * from "https://raw.githubusercontent.com/DjDeveloperr/harmony/interactions/mod.ts";
